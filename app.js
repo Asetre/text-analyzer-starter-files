b7160c1354f1dbcdf9da2cdb19864e31882045fc
Asetr <asetre.paul@gmail.com>
2016-11-14 14:17:02 -0800

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// your code here!
+
+
+$('form').submit(function (event) {
+	event.preventDefault();
+	var hash = {};
+	var userText = $('#user-text').val().split(/\s+/);
+	wordCount = userText.length;
+	var letterCount = 0;
+	var avgWordLength;
+	
+	for (var i in userText) {
+		var x = userText[i];
+		(hash[x] ? hash[x]++ : hash[x] = 1);
+		
+	}
+
+	for (var i in userText) {
+		letterCount += userText[i].length;
+	}
+
+	uniqueWordCount = Object.keys(hash).length;
+	avgWordLength = letterCount / userText.length;
+	avgWordLength = avgWordLength.toFixed(2);
+	console.log(avgWordLength);
+	console.log(hash);
+	console.log(wordCount);
+	
+
+	$('dl').removeClass("hidden");
+	$('.js-word-count').append(wordCount);
+	$('.js-unique-word-count').append(uniqueWordCount);
+	$('.average-word-length').append(avgWordLength);
+	
+
+});
